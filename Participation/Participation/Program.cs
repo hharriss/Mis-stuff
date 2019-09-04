b0f0781a804a80300f1ed8140ab0ac5b70aaa676
Harris Jones <hharrisjones@ou.edu>
2019-09-04 11:15:43 -0500

    Participation

---
+++ Participation/Participation/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Participation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime today = DateTime.Now;
+            DateTime Future = today.AddDays(26);
+            Console.WriteLine(today);
+            Console.WriteLine(today.ToLongTimeString());
+            Console.WriteLine(Future);
+
+
+            var difference = Future - today;
+            Console.WriteLine(difference.Days);
+            Console.ReadKey();
+
+        }
+    }
+}
